8363f7aae5646245943788f785a3cfc86fb3423d
Sonali Marlin <sonali.marlin@techtonic.com>
2018-10-10 16:21:00 -0600

    add changes to test landlord object

--- js/util.js
+++ js/util.js
@@ -8,117 +8,105 @@ function User(firstName, lastName, email, password, avatar) {
   this.properties = [];
 }
 
-function Property() {
-  this.address = {};
-  this.tenantInfo = {};
-  this.expenses = {};
+function Property(oAddress, oTenantInfo, oExpenses) {
+  this.address = oAddress || {};
+  this.tenantInfo = oTenantInfo || {};
+  this.expenses = oExpenses || {};
 }
 
-var testUser = new User("Patrick","Smith","patrick@yoohoo.org","password","../images/userOne.jpeg");
-
 //Property Object
-var testProperty =
-{
-  address:
-    {
-      address1: "123 Elm St.",
-      address2: "305",
-      city: "Denver",
-      state: "CO",
-      zip: 80209
-    },
-    tenantInfo:
-      {
-        firstName: "Maggie",
-        lastName: "Brown",
-        email: "maggie@abc.org",
-        monthlyRent: 1750,
-        leaseStart: "6/1/2018",
-        leaseEnd: "5/31/2019"
-      }
+var PropertyAddress1 = {
+  address1: "123 Elm St.",
+  address2: "305",
+  city: "Denver",
+  state: "CO",
+  zip: 80209
 };
 
-var expenses = {
-  expenses:
-      {
-        mileage:
-          [
-            {
-              date: "7/5/2018",
-              miles: 25
-            },
-            {
-              date: "8/18/2018",
-              miles: 25
-            }
-          ],
-        mortgage:
-          {
-            premium: 1700,
-            escrow: 800,
-            interest: 85,
-            mortgageIns: 100
-          },
-        maintenance:
-          [
-            {
-              date: "7/18/2018",
-              description:"cleaning",
-              amount:160,
-              image:"../images/receipt.jpg"
-            }
-          ],
-        suppliesGoods:
-          [
-            {
-              date: "9/01/2018",
-              description:"bathroom cleaning supplies",
-              amount:25,
-              image:"../images/receipt.jpg"
-            }
-          ],
-        misc:
-          [
-            {
-              date: "7/18/2018",
-              description:"painting living room wall",
-              amount:800,
-              image:"../images/receipt.jpg"
-            }
-          ],
-        hoa:
-          [
-            {
-              date: "6/1/2018",
-              amount: 50
-            }
-          ],
-        rent:
-          [
-            {
-              date: "6/5/2018",
-              amount: 1750
-            },
-            {
-              date: "7/2/2018",
-              amount: 1750
-            },
-            {
-              date: "8/5/2018",
-              amount: 1750
-            }
-          ]
-      }
-  };
+var tenantInfo1 = {
+  firstName: "Maggie",
+  lastName: "Brown",
+  email: "maggie@abc.org",
+  monthlyRent: 1750,
+  leaseStart: "6/1/2018",
+  leaseEnd: "5/31/2019"
+};
 
+var expenses1 = {
+  mileage: [
+    {
+      date: "7/5/2018",
+      miles: 25
+    },
+    {
+      date: "8/18/2018",
+      miles: 25
+    }
+  ],
+  mortgage: {
+    premium: 1700,
+    escrow: 800,
+    interest: 85,
+    mortgageIns: 100
+  },
+  maintenance: [
+    {
+      date: "7/18/2018",
+      description:"cleaning",
+      amount:160,
+      image:"../images/receipt.jpg"
+    }
+  ],
+  suppliesGoods: [
+    {
+      date: "9/01/2018",
+      description:"bathroom cleaning supplies",
+      amount:25,
+      image:"../images/receipt.jpg"
+    }
+  ],
+  misc: [
+    {
+      date: "7/18/2018",
+      description:"painting living room wall",
+      amount:800,
+      image:"../images/receipt.jpg"
+    }
+  ],
+  hoa: [
+    {
+      date: "6/1/2018",
+      amount: 50
+    }
+  ],
+  rent: [
+    {
+      date: "6/5/2018",
+      amount: 1750
+    },
+    {
+      date: "7/2/2018",
+      amount: 1750
+    },
+    {
+      date: "8/5/2018",
+      amount: 1750
+    }
+  ]
+};
+
+var property1 = new Property(PropertyAddress1,tenantInfo1,expenses1);
+var landlord1 = new User("Patrick","Smith","patrick@yoohoo.org","password","../images/userOne.jpeg");
+landlord1.properties.push(property1);
+var landlords = [];
+landlords.push(landlord1);
 //Localstorage stuff
-$(loadContent);
+$(loadContent(landlords));
 
-function loadContent(oUser, aProperties){
+function loadContent(aUser){
   if (typeof(Storage) !== "undefined") {
     // Store
-    localStorage.setItem("user", oUser);
-    localStorage.setItem("properties", aProperties);
+    localStorage.setItem("user", JSON.stringify(aUser));
     // Retrieve
       //document.getElementById("heading").innerHTML = localStorage.getItem("content");
   } else {
