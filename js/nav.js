cbcffe78d3d6ecf6880d54db61da0d15bcd6c48f
Sonali Marlin <sonali.marlin@techtonic.com>
2018-10-11 14:52:04 -0600

    add test object data to nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,26 @@
 $("#add-new-property").on("submit", function(e){
   e.preventDefault();
+  console.log($(this));
   
+  //$('#add-property-modal').modal('hide');
 });
+
+// var PropertyAddress1 = {
+//   address1: "123 Elm St.",
+//   address2: "#305",
+//   city: "Denver",
+//   state: "CO",
+//   zip: 80209
+// };
+//
+// var tenantInfo1 = {
+//   firstName: "Maggie",
+//   lastName: "Brown",
+//   email: "maggie@abc.org",
+//   monthlyRent: 1750,
+//   leaseStart: "6/1/2018",
+//   leaseEnd: "5/31/2019"
+// };
+
+// var property1 = new Property(PropertyAddress1,tenantInfo1,expenses1);
+//landlord1.properties.push(property1);
