8501d0b440005721f67d6a62a732608c1379b488
Sonali Marlin <sonali.marlin@techtonic.com>
2018-10-11 15:19:00 -0600

    add object creation login to nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,25 @@
 $("#add-new-property").on("submit", function(e){
   e.preventDefault();
-  console.log($(this));
-  
+  console.log($(this)[0]);
+  var PropertyAddress2 = {};
+  PropertyAddress2.address1 = $(this)[0][0].value;
+  PropertyAddress2.address2 = $(this)[0][1].value;
+  PropertyAddress2.city = $(this)[0][2].value;
+  PropertyAddress2.state = $(this)[0][3].value;
+  PropertyAddress2.zip = $(this)[0][4].value;
+  console.log(PropertyAddress2);
+
+  var tenantInfo2 = {};
+  tenantInfo2.name = $(this)[0][6].value;
+  tenantInfo2.email = $(this)[0][7].value;
+  tenantInfo2.amount = $(this)[0][8].value;
+  tenantInfo2.start = $(this)[0][9].value;
+  tenantInfo2.end = $(this)[0][9].value;
+  console.log(tenantInfo2);
   //$('#add-property-modal').modal('hide');
 });
 
-// var PropertyAddress1 = {
-//   address1: "123 Elm St.",
-//   address2: "#305",
-//   city: "Denver",
-//   state: "CO",
-//   zip: 80209
-// };
+
 //
 // var tenantInfo1 = {
 //   firstName: "Maggie",
