8442bc5aa01087412e1f6927c884506571f22bb1
Mollie Chacon <mollie.chacon@techtonic.com>
2018-10-11 15:01:49 -0600

    fix prop details and add prelim click event for edit prop

--- js/property-details.js
+++ js/property-details.js
@@ -1,8 +1,11 @@
+// PROPERTY DETAILS SECTION
+
 console.log(landlord1.properties[0]);
 
 var street = landlord1.properties[0].address.address1;
 var street2 = landlord1.properties[0].address.address2;
 var cityStateZip = landlord1.properties[0].address.city + ", " + landlord1.properties[0].address.state + " " + landlord1.properties[0].address.zip;
+
 var tenantName = "Current Tenant: " + landlord1.properties[0].tenantInfo.firstName + " " + landlord1.properties[0].tenantInfo.lastName;
 var displayTenantEmail = "Current Tenant Email: " + landlord1.properties[0].tenantInfo.email;
 var monthlyRentAmount = " Monthly Rental Amount: $" + landlord1.properties[0].tenantInfo.monthlyRent;
@@ -14,6 +17,9 @@ var getTenantEmail = landlord1.properties[0].tenantInfo.email;
 var rentalAmount = landlord1.properties[0].tenantInfo.monthlyRent;
 var leaseStartDate = landlord1.properties[0].tenantInfo.leaseStart;
 var leaseEndDate = landlord1.properties[0].tenantInfo.leaseEnd;
+var getCity = landlord1.properties[0].address.city;
+var getState = landlord1.properties[0].address.state;
+var getZip = landlord1.properties[0].address.zip;
 
 
 // Display property info
@@ -29,6 +35,11 @@ $('#display-lease-range').text(leaseDateRange);
 // Edit property info Modal
 $('input[name="edit-prop-address-street"]').val(street);
 $('input[name="edit-prop-address-street2"]').val(street2);
+
+$('input[name="edit-prop-address-city"]').val(getCity);
+$('input[name="edit-prop-address-state"]').val(getState);
+$('input[name="edit-prop-address-zip"]').val(getZip);
+
 $('input[name="edit-tenant-first-name"]').val(getTenantFirst);
 $('input[name="edit-tenant-last-name"]').val(getTenantLast);
 $('input[name="edit-tenant-email"]').val(getTenantEmail);
@@ -37,7 +48,7 @@ $('input[name="edit-lease-start"]').val(leaseStartDate);
 $('input[name="edit-lease-end"]').val(leaseEndDate);
 
 // onclick event for submit edit property
-// select button or form w/ jquery?
+// should I target the button or form tag w/ jquery?
 // issues with updating 2 obj? PropertyAddress1 and tenantInfo1
 // not sure how to test this!! MC
 
@@ -45,12 +56,16 @@ $("#submit-edit-property").on("submit", function(e){
   e.preventDefault();
   var address1 = $('input[name="edit-prop-address-street"]').val();
   var address2 = $('input[name="edit-prop-address-street2"]').val();
+  var city = $('input[name="edit-prop-address-city"]').val();
+  var state = $('input[name="edit-prop-address-state"]').val();
+  var zip = $('input[name="edit-prop-address-zip"]').val();
   var firstName = $('input[name="edit-tenant-first-name"]').val();
   var lastName = $('input[name="edit-tenant-last-name"]').val();
   var email = $('input[name="edit-tenant-email"]').val();
   var monthlyRent = $('input[name="edit-rental-amount"]').val();
   var leaseStart = $('input[name="edit-lease-start"]').val();
   var leaseEnd = $('input[name="edit-lease-end"]').val();
+
 
   // var updatedPropertyAddress =
   //
@@ -59,3 +74,6 @@ $("#submit-edit-property").on("submit", function(e){
   // alert("A new landlord user was created in localStorage");
   // updateState(landlords);
 });
+
+
+// EXPENSES SECTION
