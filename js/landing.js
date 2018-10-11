fd5b4a631302b3cea29868b809054dff8c09009e
Brent Smith <brent.smith@techtonic.com>
2018-10-11 16:35:24 -0600

    Add on click functionality to 'Sign in' button and match sign in info to stored User object

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,4 @@
-$("#signupForm").on("submit", function(e){
+$('#signupForm').on('submit', function (e) {
   e.preventDefault();
   var firstName = $('input[name="signUpFirstName"]').val();
   var lastName = $('input[name="signUpLastName"]').val();
@@ -6,6 +6,19 @@ $("#signupForm").on("submit", function(e){
   var password = $('input[name="signUpPassword"]').val();
   var newLandlord = new User(firstName, lastName, email, password);
   landlords.push(newLandlord);
-  alert("A new landlord user was created in localStorage");
+  alert('A new landlord user was created in localStorage');
   updateState(landlords);
+});
+
+$('#signinForm').on('submit', function (e) {
+  e.preventDefault();
+  for (var i = 0; i < landlords.length; i++) {
+    if (landlords[i].email === $('#signInEmail').val() &&  landlords[i].password === $('#signInPassword').val())
+    {
+      location.href = 'dashboard.html';
+      return;
+    }
+  }
+
+  alert('Email or Password do not match');
 });
